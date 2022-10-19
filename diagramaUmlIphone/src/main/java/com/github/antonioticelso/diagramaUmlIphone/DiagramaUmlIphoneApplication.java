083d2617ebbd12ff44bb02ced6618289f6aac830
Antonio <antonio.ti.celso@gmail.com>
2022-10-19 16:08:14 -0300

    DiagramaUmlIphone - First commit

---
+++ diagramaUmlIphone/src/main/java/com/github/antonioticelso/diagramaUmlIphone/DiagramaUmlIphoneApplication.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.diagramaUmlIphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagramaUmlIphoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagramaUmlIphoneApplication.class, args);
+	}
+
+}
---
+++ diagramaUmlIphone/src/test/java/com/github/antonioticelso/diagramaUmlIphone/DiagramaUmlIphoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.diagramaUmlIphone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagramaUmlIphoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
