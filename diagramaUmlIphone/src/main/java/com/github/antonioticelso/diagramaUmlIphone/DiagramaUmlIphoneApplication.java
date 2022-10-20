6d086f77a84a5b6e15d0e35779d2464fd153408e
Antonio <antonio.ti.celso@gmail.com>
2022-10-20 16:44:13 -0300

    AparelhoInternet - Implementação de metodos

--- diagramaUmlIphone/src/main/java/com/github/antonioticelso/diagramaUmlIphone/DiagramaUmlIphoneApplication.java
+++ diagramaUmlIphone/src/main/java/com/github/antonioticelso/diagramaUmlIphone/DiagramaUmlIphoneApplication.java
@@ -1,5 +1,6 @@
 package com.github.antonioticelso.diagramaUmlIphone;
 
+import com.github.antonioticelso.diagramaUmlIphone.model.AparelhoInternet;
 import com.github.antonioticelso.diagramaUmlIphone.model.AparelhoMusical;
 import com.github.antonioticelso.diagramaUmlIphone.model.AparelhoTelefonico;
 import org.springframework.boot.SpringApplication;
@@ -11,21 +12,29 @@ public class DiagramaUmlIphoneApplication {
 	public static void main(String[] args) {
 
 		AparelhoMusical musical = new AparelhoMusical();
+		AparelhoTelefonico telefonico = new AparelhoTelefonico();
+		AparelhoInternet internet = new AparelhoInternet();
 
 		musical.getPlay();
 		musical.getPause();
 		musical.getPause();
 		musical.getSelect();
 
-		AparelhoTelefonico telefonico = new AparelhoTelefonico();
-
 		telefonico.getCall(98833664);
 		telefonico.getCall(988336647);
 		telefonico.getAtender(false);
 		telefonico.getAtender(true);
 
+		internet.getAddPage();
+		internet.getAddPage();
+		internet.getAddPage();
+		internet.getUpPage(3);
+		internet.getUpPage(2);
+		internet.getUpPage(0);
+		internet.getUpPage(7);
 
 //		SpringApplication.run(DiagramaUmlIphoneApplication.class, args);
+
 	}
 
 }
--- diagramaUmlIphone/src/main/java/com/github/antonioticelso/diagramaUmlIphone/model/AparelhoInternet.java
+++ diagramaUmlIphone/src/main/java/com/github/antonioticelso/diagramaUmlIphone/model/AparelhoInternet.java
@@ -2,6 +2,29 @@ package com.github.antonioticelso.diagramaUmlIphone.model;
 
 public class AparelhoInternet {
 
-//    exibirPagina, addNovaAba, atualizarPagina
+    int numberPage = 0;
+
+    public void getAddPage() {
+        if (numberPage == 0) {
+            getReadPage();
+            numberPage++;
+        } else {
+            System.out.println("Nova página " + numberPage);
+            getReadPage();
+            numberPage++;
+        }
+    }
+
+    public void getUpPage(int numbers) {
+        if (numbers <= numberPage) {
+            System.out.println("Atualizando página: " + numbers);
+        } else {
+            System.out.println("Página: " + numbers + " não existe!");
+        }
+    }
+
+    private void getReadPage() {
+        System.out.println("Iniciando página!");
+    }
 
 }
