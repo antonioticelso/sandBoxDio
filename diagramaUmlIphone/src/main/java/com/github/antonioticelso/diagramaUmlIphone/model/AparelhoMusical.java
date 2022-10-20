17d57de8b6a30330ac3f3afd9b6a73a0c783202d
Antonio <antonio.ti.celso@gmail.com>
2022-10-20 15:05:28 -0300

    AparelhoMusical - Implementação de metodos

--- diagramaUmlIphone/src/main/java/com/github/antonioticelso/diagramaUmlIphone/model/AparelhoMusical.java
+++ diagramaUmlIphone/src/main/java/com/github/antonioticelso/diagramaUmlIphone/model/AparelhoMusical.java
@@ -2,10 +2,37 @@ package com.github.antonioticelso.diagramaUmlIphone.model;
 
 public class AparelhoMusical {
 
-    //    tocar, pausar, selecionar
+    boolean stop = false;
+    boolean pauser = false;
 
-    public void getTocar() {
+    public void getPlay() {
         System.out.println("Tocando música escolhida!");
+    }
+
+    public void getPause() {
+        if (!pauser) {
+            System.out.println("Pausou a música!");
+            pauser = true;
+        } else {
+            System.out.println("Continuando música de onde pausou!");
+            pauser = false;
+        }
+    }
+
+    public void getStop() {
+        if (!pauser) {
+            System.out.println("Stop!");
+            stop = true;
+        } else {
+            getPlay();
+            System.out.println("Iniciando música!");
+            stop = false;
+        }
+    }
+
+    public void getSelect() {
+        System.out.println("Selecione a música desejada!");
+        getPlay();
     }
 
 }
