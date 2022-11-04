2a203bce44ce5bf027c2785a20a859f1897d4083
Antonio <antonio.ti.celso@gmail.com>
2022-11-04 01:07:30 -0300

    Remocao de dependency jakarta

--- cloudParking/src/main/java/com/github/antonioticelso/cloudParking/repository/ParkingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.antonioticelso.cloudParking.repository;
-
-import com.github.antonioticelso.cloudParking.model.Parking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParkingRepository extends JpaRepository<Parking, String> {
-}
