3c0826fd089dee12c84ae1edf258e76422ae59ae
Antonio <antonio.ti.celso@gmail.com>
2022-11-03 21:23:07 -0300

    First commit sala de reuniao

---
+++ salaReuniao/src/test/java/com/github/antonioticelso/salaReuniao/SalaReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.salaReuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaReuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
