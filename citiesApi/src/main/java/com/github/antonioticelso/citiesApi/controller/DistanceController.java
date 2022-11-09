7a6e732052b71c9e4024e1c6cfb5fc36c916898a
Antonio <antonio.ti.celso@gmail.com>
2022-11-09 15:58:05 -0300

    Create paramiters distancies - CityApi

---
+++ citiesApi/src/main/java/com/github/antonioticelso/citiesApi/controller/DistanceController.java
@@ -0,0 +1,45 @@
+package com.github.antonioticelso.citiesApi.controller;
+
+import com.github.antonioticelso.citiesApi.service.DistanceService;
+import com.github.antonioticelso.citiesApi.utils.EarthRadius;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/distances")
+public class DistanceController {
+
+    private final DistanceService service;
+    Logger log = LoggerFactory.getLogger(DistanceController.class);
+
+    public DistanceController(DistanceService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/by-points")
+    public Double byPoints(@RequestParam(name = "from") final Long city1,
+                           @RequestParam(name = "to") final Long city2) {
+        log.info("byPoints");
+        return service.distanceByPointsInMiles(city1, city2);
+    }
+
+    @GetMapping("/by-cube")
+    public Double byCube(@RequestParam(name = "from") final Long city1,
+                         @RequestParam(name = "to") final Long city2) {
+        log.info("byCube");
+        return service.distanceByCubeInMeters(city1, city2);
+    }
+
+    @GetMapping("/by-math")
+    public Double byMath(@RequestParam(name = "from") final Long city1,
+                         @RequestParam(name = "to") final Long city2,
+                         @RequestParam final EarthRadius unit) {
+        log.info("byMath");
+        return service.distanceUsingMath(city1, city2, unit);
+    }
+
+}
